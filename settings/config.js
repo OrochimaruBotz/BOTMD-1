d135c54bc07df05712e5f408f67d644c75f36ef5
Humanz <109642842+OrochimaruBotz@users.noreply.github.com>
2022-11-10 15:29:24 +0700

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -12,11 +12,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283160624440','6282279915237']
-global.premium = ['6282279915237']
-global.botname = 'Feyza Botz'
-global.ownername = 'Feyza Botz'
-global.packname = 'Feyza'
+global.owner = ['6288804947282']
+global.premium = ['6288804947282']
+global.botname = 'GojoBotzMD'
+global.ownername = 'GojoBotzMD'
+global.packname = 'GojoBotzMD'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
